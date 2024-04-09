6bc0b5420fb7eb35a668e3018b304a8adc16a587
VuthSarandy <dysart9999@gmail.com>
2024-04-09 15:06:01 +0700

    test

---
+++ src/main/java/co/istad/testremoveorigin/TestRemoveOriginApplication.java
@@ -0,0 +1,13 @@
+package co.istad.testremoveorigin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRemoveOriginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRemoveOriginApplication.class, args);
+    }
+
+}
